8a9a5acd1b4b7bee3ec2e4157bbd248bb8946928
syedibrahimhamza <ibrahimhamza1211@gmail.com>
2026-09-13 10:25:59 -0700

    Add restaurant_bill_generator function with menu list

---
+++ restaurant_bill_generator.py
@@ -0,0 +1,9 @@
+def restaurant_bill_generator():
+    menu = [
+        ("Burger", 350),
+        ("Pizza", 900),
+        ("Pasta", 650),
+        ("Fries", 200),
+        ("Cold Drink", 100),
+        ("Sandwich", 300),
+    ]
